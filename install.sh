5e8568785d86f088778b13a226c0943f4cbbcfc4
Łukasz Górski <moon.musick@gmail.com>
2015-07-12 14:19:31 +0200

    Cleanup install script

--- install.sh
+++ install.sh
@@ -2,16 +2,12 @@
 
 BASEDIR="$(dirname ${BASH_SOURCE[0]})"
 
-cd "${BASEDIR}"
-git submodule update --init --recursive
 # gem install rcodetools
+
 cd "${BASEDIR}/bundle/YouCompleteMe"
 ./install.sh
+
 cd "${BASEDIR}/bundle/ctrlp-cmatcher"
 ./install.sh
-
-# custom colorscheme for airline
-# cp "${BASEDIR}/moonmusick.vim" \
-#   "${BASEDIR}/bundle/vim-airline/autoload/airline/themes/"
 
 touch "${BASEDIR}/install.lock"
