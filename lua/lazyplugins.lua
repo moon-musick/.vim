a2b0952e44d61c11d2e375a0e6b84a7aff671026
Łukasz Górski <moon.musick@gmail.com>
2024-07-24 17:27:38 +0200

    Add aerial.nvim (code outline window, tree-sitter or LSP dependent)

--- lua/lazyplugins.lua
+++ lua/lazyplugins.lua
@@ -96,6 +96,15 @@ require("lazy").setup({
 
   "w0rp/ale",
 
+  {
+    "stevearc/aerial.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+
   -- folding ------------------------------------------------------------------
 
   -- custom folding formatting (indentation-based)
@@ -361,3 +370,15 @@ require('lualine').setup({
     section_separators = '',
   }
 })
+
+-- aerial.nvim
+require("aerial").setup({
+  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+  on_attach = function(bufnr)
+    -- Jump forwards/backwards with '{' and '}'
+    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+  end,
+})
+
+vim.keymap.set("n", "<leader>ae", "<cmd>AerialToggle!<CR>")
